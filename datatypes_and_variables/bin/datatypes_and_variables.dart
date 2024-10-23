96ddc50c549cd945f1c92babcb390f87543d0384
its-me-depk <pandey129003@gmail.com>
2024-10-23 22:49:28 +0530

    add dart project

---
+++ datatypes_and_variables/bin/datatypes_and_variables.dart
@@ -0,0 +1,5 @@
+import 'package:datatypes_and_variables/datatypes_and_variables.dart' as datatypes_and_variables;
+
+void main(List<String> arguments) {
+  print('Hello world: ${datatypes_and_variables.calculate()}!');
+}
---
+++ datatypes_and_variables/test/datatypes_and_variables_test.dart
@@ -0,0 +1,8 @@
+import 'package:datatypes_and_variables/datatypes_and_variables.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
